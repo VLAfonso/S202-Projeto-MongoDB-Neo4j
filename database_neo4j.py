1529f04b7c4e746d57cea1b74381248a9b364ce2
Virginia Leticia Afonso <vlafonso@hotmail.com>
2025-06-05 18:40:17 -0300

    Add inserção dataset

--- database_neo4j.py
+++ database_neo4j.py
@@ -1,8 +1,10 @@
 from neo4j import GraphDatabase
+from dataset_neo4j import Dataset
 
 class Database:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.dataset = Dataset(self)
 
     def close(self):
         self.driver.close()
@@ -18,3 +20,9 @@ class Database:
     def drop_all(self):
         with self.driver.session() as session:
             session.run("MATCH (n) DETACH DELETE n")
+    
+    def insert_dataset(self):
+        # Apagar todos os dados
+        self.drop_all()
+        # Inserir dados base do banco
+        self.dataset.insert_dataset()
