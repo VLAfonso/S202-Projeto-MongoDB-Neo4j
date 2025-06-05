ddc85fecf197d5cfff56e7af63e0c32835e1aa72
Virginia Leticia Afonso <vlafonso@hotmail.com>
2025-06-05 18:42:50 -0300

    Add informações para dataset

---
+++ dataset_mongodb.py
@@ -0,0 +1,72 @@
+dataset = [
+    {
+        "id": 0,
+        "nome": "Arroz 5kg",
+        "codigoBarras": 7896436750028,
+        "preco": 25.99,
+        "quantidade": 215
+    },
+    {
+        "id": 1,
+        "nome": "Feijão 2kg",
+        "codigoBarras": 7895673500982,
+        "preco": 15.60,
+        "quantidade": 62
+    },
+    {
+        "id": 2,
+        "nome": "Açúcar 5kg",
+        "codigoBarras": 7896720275231,
+        "preco": 16.99,
+        "quantidade": 513 
+    },
+    {
+        "id": 3,
+        "nome": "Macarrão Parafuso 500g",
+        "codigoBarras": 7899785462351,
+        "preco": 5.50,
+        "quantidade": 71
+    },
+    {
+        "id": 4,
+        "nome": "Café 500g",
+        "codigoBarras": 7894562587956,
+        "preco": 29.99,
+        "quantidade": 203
+    },
+    {
+        "id": 5,
+        "nome": "Óleo de Soja 900ml",
+        "codigoBarras": 7899856243215,
+        "preco": 6.99,
+        "quantidade": 1003
+    },
+    {
+        "id": 6,
+        "nome": "Sal 1kg",
+        "codigoBarras": 7891452567893,
+        "preco": 2.50,
+        "quantidade": 151 
+    },
+    {
+        "id": 7,
+        "nome": "Sabonete 90g",
+        "codigoBarras": 7898956421357,
+        "preco": 1.89,
+        "quantidade": 113
+    },
+    {
+        "id": 8,
+        "nome": "Creme Dental 150g",
+        "codigoBarras": 7894852126587,
+        "preco": 6.50,
+        "quantidade": 208
+    },
+    {
+        "id": 9,
+        "nome": "Papel Higiênico 4un",
+        "codigoBarras": 7895486235149,
+        "preco": 7.50,
+        "quantidade": 126
+    },
+]
