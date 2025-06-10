f3ba9f78db1d3488fa09625e01517d4241a1da5d
Virginia Leticia Afonso <vlafonso@hotmail.com>
2025-06-10 15:52:21 -0300

    Add conexão com o MongoDB

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from cli import InicialCLI, EstoqueCLI, FuncionariosCLI
 from database_mongodb import DatabaseMongoDB
 from database_neo4j import DatabaseNeo4j
 from funcionarios import FuncionariosCRUD
+from estoque import EstoqueCRUD
 
-#dbMongo = DatabaseMongoDB("Supermercado", "Estoque")
+# Criar banco de dados do estoque
+dbMongoDB = DatabaseMongoDB(database='Supermercado', collection='Estoque')
+dbMongoDB.resetDatabase()
+
+# Criar banco de dados dos funcionários e setores
 dbNeo4j = DatabaseNeo4j("neo4j://localhost:7687", "neo4j", "neo4jneo4j")
-# dbNeo4j.insert_dataset()
+dbNeo4j.insert_dataset()
 
+# Criar instância do CRUD de estoque, funcionários e setores
+estoque_crud = EstoqueCRUD(dbMongoDB)
 funcionarios_crud = FuncionariosCRUD(dbNeo4j)
-# estoque_crud = EstoqueCRUD(dbMongo)
 
-cli = InicialCLI("estoque", funcionarios_crud)
-
+# Criar e iniciar CLI
+cli = InicialCLI(estoque_crud, funcionarios_crud)
 cli.run()
