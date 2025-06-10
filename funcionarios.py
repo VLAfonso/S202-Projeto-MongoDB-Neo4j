21c5f3e612abe455455219cea3308c56f3af4a0f
Virginia Leticia Afonso <vlafonso@hotmail.com>
2025-06-10 14:46:31 -0300

    Add relacionamentos

--- funcionarios.py
+++ funcionarios.py
@@ -2,11 +2,12 @@ class FuncionariosCRUD:
     def __init__(self, database):
         self.db = database
 
+    # CRUD do funcionário
     def criar_funcionario(self, nome, dataNascimento, cpf, id, telefone, email):
         query = "CREATE (:Funcionario {nome: $nome, dataNascimento: $dataNascimento, cpf: $cpf, id: $id, telefone: $telefone, email: $email})"
         result = self.db.execute_query(query, {'nome': nome, 'dataNascimento': dataNascimento, 'cpf': cpf, 'id': id, 'telefone': telefone, 'email': email})
         if result is not None:
-            print(f'O funcionario {nome} foi criado no banco de dados')
+            print(f'O funcionário {nome} foi criado no banco de dados')
     
     def ler_funcionario(self, id):
         query = "MATCH (f:Funcionario {id: $id}) RETURN f.nome as nome, f.dataNascimento as dataNascimento, f.cpf as cpf, f.telefone as telefone, f.email as email"
@@ -27,14 +28,15 @@ class FuncionariosCRUD:
         query = "MATCH (f:Funcionario {id: $id}) SET f.telefone = $telefone, f.email = $email"
         result = self.db.execute_query(query, {'id': id, 'telefone': telefone, 'email': email})
         if result is not None:
-            print(f'O funcionario com id: {id} foi alterado, novo telefone: {telefone} e novo email: {email}')
+            print(f'O funcionário com id: {id} foi alterado, novo telefone: {telefone} e novo email: {email}')
     
     def apagar_funcionario(self, id):
         query = "MATCH (f:Funcionario {id: $id}) DETACH DELETE f"
         result = self.db.execute_query(query, {'id': id})
         if result is not None:
-            print(f'O funcionario com id: {id} foi deletado do banco de dados')
+            print(f'O funcionário com id: {id} foi deletado do banco de dados')
 
+    # CRUD do setor
     def criar_setor(self, nome, id):
         query = "CREATE (:Setor {id: $id, nome: $nome})"
         result = self.db.execute_query(query, {'id': id, 'nome': nome})
@@ -63,3 +65,17 @@ class FuncionariosCRUD:
         result = self.db.execute_query(query, {'id': id})
         if result is not None:
             print(f'O setor com id: {id} foi deletado do banco de dados')
+
+    # Relacionar setor e funcionário
+    def funcionario_setor(self, funcionario, setor):
+        query = "MATCH (f:Funcionario {nome: $nome_funcionario}), (s:Setor {nome: $nome_setor}) CREATE (f)-[:TRABALHA_EM]->(s)"
+        result = self.db.execute_query(query, {'nome_funcionario': funcionario, 'nome_setor': setor})
+        if result is not None:
+            print(f'O relacionamento entre o funcionário {funcionario} e o setor {setor} foi criado no banco de dados')
+
+    # Relacionar funcionário e gerente
+    def funcionario_gerente(self, funcionario, gerente):
+        query = "MATCH (f:Funcionario {nome: $nome_funcionario}), (g:Funcionario {nome: $nome_gerente}) CREATE (g)-[:GERENCIA]->(f)"
+        result = self.db.execute_query(query, {'nome_funcionario': funcionario, 'nome_gerente': gerente})
+        if result is not None:
+            print(f'O relacionamento entre o gerente {gerente} e o funcionário {funcionario} foi criado no banco de dados')
